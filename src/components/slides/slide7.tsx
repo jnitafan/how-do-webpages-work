9e9401e30898458bea790835fe6fbc099e8a6e35
Jacob Nitafan <jacob.nitafan@gmail.com>
2025-05-05 16:06:45 +1000

    lottie anims

--- src/components/slides/slide7.tsx
+++ src/components/slides/slide7.tsx
@@ -1,7 +1,14 @@
 // components/slides/Slide7.tsx
 "use client";
 
-import React, { forwardRef, useImperativeHandle } from "react";
+import React, {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useState,
+} from "react";
+import dynamic from "next/dynamic";
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 import styles from "./slides.module.scss";
 
 const Slide7 = forwardRef((_, ref) => {
@@ -10,9 +17,31 @@ const Slide7 = forwardRef((_, ref) => {
     exitAnimation: () => Promise.resolve(),
   }));
 
+  const [routingAnim, setRoutingAnim] = useState(null);
+  const [headersAnim, setHeadersAnim] = useState(null);
+
+  useEffect(() => {
+    // Fetch the animation JSON file from the public folder
+    fetch("/routing.json")
+      .then((response) => response.json())
+      .then((data) => setRoutingAnim(data))
+      .catch((error) =>
+        console.error("Error loading the Lottie animation:", error)
+      );
+    fetch("/headers.json")
+      .then((response) => response.json())
+      .then((data) => setHeadersAnim(data))
+      .catch((error) =>
+        console.error("Error loading the Lottie animation:", error)
+      );
+  }, []);
+
   return (
     <div className={styles.slide}>
-      <div></div>
+      <div>
+        <Lottie animationData={routingAnim} loop={true} />
+        <Lottie animationData={headersAnim} loop={false} />
+      </div>
     </div>
   );
 });
